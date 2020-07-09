fcbdea334c99208db9bb6b95ce8b37cf0c8c0334
Karim Ait-Allaoua <kaitallaoua@gmail.com>
2020-07-08 18:40:04 -0600

    started error handling
    added support for when the number of sabertooths and motors to control are mismatched. Still need to properly handle these errors along with others

--- package/multi-saber-serial.py
+++ package/multi-saber-serial.py
@@ -1,62 +1,77 @@
 class MultiSaberLegacy:
 
-
+#https://code.tutsplus.com/tutorials/professional-error-handling-with-python--cms-25950
 	#private - should not be available to user
 	MOTOR1 = 0
 	MOTOR2 = 4
 
 	uartMsg = "" # initalize empty uart Msg
 
-
-
-	def __init__(self, addresses, baudrates):
+	def __init__(self, addresses, enableFailsafe ):
 		self.addresses = addresses
-		self.baudrates = baudrates		
+		self.enableFailsafe = enableFailsafe			
 
 	def driveMotors(self, speeds):
 		self.uartMsg = "" # reset uart msg buffer
 
-		for isaber, address in enumerate(self.addresses): # for each sabertooth
+		lenOfSabers = len(self.addresses)
 
-			# are we on the last sabertooth?
-			notLastSaber = len(self.addresses) - 1 != isaber
+		lenOfSpeeds = len(speeds)
 
-			# is there an even number of motors to control?
-			evenNumOfMotors = len(speeds) % 2 == 0
+		nOfSabersEqualSpeeds = lenOfSabers * 2 != lenOfSpeeds
+		nOfSabers1EqualSpeeds = lenOfSabers * 2 - 1 != lenOfSpeeds
 
-			if notLastSaber or evenNumOfMotors:
-				availableMotors = 2
-			else:
-				availableMotors = 1 
+		print(nOfSabersEqualSpeeds)
+		print(nOfSabers1EqualSpeeds)
 
-			for imotor in range(availableMotors):
-				if imotor == 0:
-					motorCommand = self.MOTOR1
+		if nOfSabersEqualSpeeds ^ nOfSabers1EqualSpeeds:
+
+			for isaber, address in enumerate(self.addresses): # for each sabertooth
+
+				# are we on the last sabertooth?
+				notLastSaber = lenOfSabers - 1 != isaber
+
+				# is there an even number of motors to control?
+				evenNumOfMotors = lenOfSpeeds % 2 == 0
+
+				if notLastSaber or evenNumOfMotors:
+					availableMotors = 2
 				else:
-					motorCommand = self.MOTOR2
+					availableMotors = 1 
+
+				for imotor in range(availableMotors):
+					if imotor == 0:
+						motorCommand = self.MOTOR1
+					else:
+						motorCommand = self.MOTOR2
+						
+					cmdData = self.speedToCmd(speeds[(isaber * 2) + imotor], motorCommand)
+
+					print("Address: " + str(address) + "\n")
 					
-				cmdData = self.speedToCmd(speeds[(isaber * 2) + imotor], motorCommand)
+					command = cmdData.get("command")
+					print("Command: " + str(command) + "\n")
 
-				print("Address: " + str(address) + "\n")
-				
-				command = cmdData.get("command")
-				print("Command: " + str(command) + "\n")
+					data = cmdData.get("data")
+					print("Data: " + str(data) + "\n")
 
-				data = cmdData.get("data")
-				print("Data: " + str(data) + "\n")
+					checksum = self.checksum(address, command, data)
+					print("Checksum: " + str(checksum) + "\n" + "----------------------")
 
-				checksum = (address + int(command) + int(data)) & 0b01111111
-				print("Checksum: " + str(checksum) + "\n" + "----------------------")
+					self.uartMsg += str(address) + str(command) + str(cmdData.get("data")) + str(checksum)
+	                                
 
-				self.uartMsg += str(address) + str(command) + str(cmdData.get("data")) + str(checksum)
-                                
+			# python write uart message
+			return bytearray(self.uartMsg, "utf-8")
 
-		# python write uart message
-		return bytearray(self.uartMsg, "utf-8")
+		else: # Mismatch too many or too litte motor speeds for the amount of sabertooths to control
 
-
+			if enableFailsafe:
+				pass
 
 
+
+			print("Mismatch of Motors to control compared to available Sabertooths")
 	def speedToCmd(self, speed, motorCmd): # take [-1.0, 1.0] speeds to command, data parameters
 		# motor should be either 1 or 2
 		# speed is a float in range [-1.0, 1.0]
@@ -73,10 +88,18 @@ class MultiSaberLegacy:
 
 		return {"command":command, "data":data}
 
+	def checksum(self, address, command, data):
+		return (address + int(command) + int(data)) & 0b01111111
+
 import serial
 #https://pyserial.readthedocs.io/en/latest/pyserial_api.html
 instance = MultiSaberLegacy([128, 129, 130], [115200])
 
-print(instance.driveMotors([-0.4, 0.3, 0.4, -0.9, 1.0, 0.2]))
+instance.driveMotors([-0.4, 0.3, 0.4, -0.9, 1.0, 0.3])
+
+print("---------------------------------------------------")
+
+instance.driveMotors([0.2, -0.3, 0.423, -0.9, -0.2])
+
 
 
